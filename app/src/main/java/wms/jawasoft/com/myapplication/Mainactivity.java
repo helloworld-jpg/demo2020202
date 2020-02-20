f2e6e769af07bee7f593eb37a53ed05a36b9a083
liuyuanye <1494512080@qq.com>
2020-02-20 14:00:46 +0800

    第一次提交0

---
+++ app/src/main/java/wms/jawasoft/com/myapplication/Mainactivity.java
@@ -0,0 +1,14 @@
+package wms.jawasoft.com.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class Mainactivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
